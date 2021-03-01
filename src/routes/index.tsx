9dbc4f1763545a4aae62d2da8d4e63fd3790a1fb
ruanduarte03 <ruanduarte0300@gmail.com>
2021-03-01 11:24:23 -0300

    create project and initial settings

---
+++ src/routes/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Home from '../containers/home'
+
+const Routes: React.FC = () => {
+  return (
+    <Home />
+  )
+}
+
+export default Routes
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box
+  }
+  html, body, #root {
+    height: 100%;
+    background: rgb(2,0,36);
+    background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,19,121,1) 34%, rgba(0,212,255,1) 92%);
+    color: white
+  }
+  *, button, input {
+    border: 0;
+    outline: 0;
+    font-family: "Roboto", sans-serif
+  }
+  button {
+    cursor: pointer;
+  }
+`
